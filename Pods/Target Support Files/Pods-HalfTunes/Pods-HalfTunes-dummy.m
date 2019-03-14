1dc1a604a3a5a6fe510a511e870676f1552caf1c
Nick Shi <nick@prevailhealth.com>
2019-03-14 17:39:37 -0500

    add cocoapod

--- Pods/Target Support Files/Pods-HalfTunes/Pods-HalfTunes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HalfTunes : NSObject
-@end
-@implementation PodsDummy_Pods_HalfTunes
-@end
--- Pods/Target Support Files/Pods-HalfTunesShowTests/Pods-HalfTunesShowTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HalfTunesShowTests : NSObject
-@end
-@implementation PodsDummy_Pods_HalfTunesShowTests
-@end
